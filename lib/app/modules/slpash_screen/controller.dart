a3cb2bf1ce96858cb37aee436dccf595ebceded8
Prakhar Jaiswal <prajai1801@gmail.com>
2023-04-10 05:02:59 +0530

    splash screen login check

--- lib/app/modules/slpash_screen/controller.dart
+++ lib/app/modules/slpash_screen/controller.dart
@@ -1,13 +1,33 @@
 import 'dart:async';
+import 'package:apply_course/app/routes/routes.dart';
 import 'package:get/get.dart';
 
+import '../../data/models/user_model.dart';
+import '../../data/providers/storage_provider.dart';
 
 class SplashController extends GetxController {
   // UserModel? user;
   late Timer _timer;
   @override
   void onInit() {
+    _timer = Timer(Duration(seconds: 3), () {
+      _validateAuthentication();
+    });
     super.onInit();
+  }
+
+  void _validateAuthentication() async {
+    StorageProvider _storage = StorageProvider();
+    try {
+      UserModel userModel = await _storage.readUserModel();
+      if (userModel.emailVerified ?? false) {
+        Get.offAllNamed(AppRoutes.homeScreen);
+      } else {
+        Get.offAllNamed(AppRoutes.loginScreen);
+      }
+    } catch (e) {
+      Get.snackbar("Error", e.toString());
+    }
   }
 
   @override
